097cd5497efc41679f4eed6e9d0405fb9f63e39d
root <hammadus@gmail.com>
2025-02-03 23:16:10 -0500

    Upload implementation with llama.cpp and DeepSeek

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PAM
 Personal Assistance Machine
+
+# Quickstart
+
+1. `docker build -t pam docker/`
+2. `sudo docker run -d -v .:/data/ -p 10000:10000 pam`
---
+++ docker/entrypoint.sh
@@ -0,0 +1,6 @@
+cd /llama.cpp/build/bin/
+./llama-server \
+    --model /data/DeepSeek-R1-GGUF/DeepSeek-R1-UD-Q2_K_XL/DeepSeek-R1-UD-Q2_K_XL-00001-of-00005.gguf \
+    --port 10000 \
+    --ctx-size 2048 \
+    --host 0.0.0.0
