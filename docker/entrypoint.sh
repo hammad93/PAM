0969157e30f625b8f6c58b9a8030b2663d65cc9b
root <hammadus@gmail.com>
2025-02-04 00:52:44 -0500

    Increase context window to 8k, maxing out current server memory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,5 @@ cd /llama.cpp/build/bin/
 ./llama-server \
     --model /data/DeepSeek-R1-GGUF/DeepSeek-R1-UD-Q2_K_XL/DeepSeek-R1-UD-Q2_K_XL-00001-of-00005.gguf \
     --port 10000 \
-    --ctx-size 2048 \
+    --ctx-size 8192 \
     --host 0.0.0.0
