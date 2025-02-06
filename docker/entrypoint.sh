af0d5381f4269f3e71c478019d3e6e18dae9080d
root <hammadus@gmail.com>
2025-02-05 19:19:11 -0500

    Increase the number of threads based on a EPYC 9334 CPU

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,5 @@ cd /llama.cpp/build/bin/
     --model /data/DeepSeek-R1-GGUF/DeepSeek-R1-UD-Q2_K_XL/DeepSeek-R1-UD-Q2_K_XL-00001-of-00005.gguf \
     --port 10000 \
     --ctx-size 8192 \
+    --threads 64 \
     --host 0.0.0.0
