018fbcf43ffaa1e1e0812009e84576205ae91149
Hammad Usmani <hammadus@gmail.com>
2025-10-19 05:13:52 +0000

    Change LLM to match current environment

--- docker/llm/entrypoint.sh
+++ docker/llm/entrypoint.sh
@@ -1,6 +1,6 @@
 cd /llama.cpp/build/bin/
 ./llama-server \
-    --model /data/swiss-ai.Apertus-70B-Instruct-2509.Q8_0-00001-of-00006.gguf \
+    --model /data/Apertus-8B-Instruct-2509-UD-Q8_K_XL.gguf \
     --port 10000 \
     --ctx-size 4096 \
     --threads 64 \
